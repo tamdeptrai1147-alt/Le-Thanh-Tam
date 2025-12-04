ac07492ba4a46dc819189ddf6b10e8ec6067d7ba
tamdeptrai1147-alt <tamdeptrai1147@gmail.com>
2025-12-04 12:31:37 +0700

    SS

--- view/footer.php
+++ view/footer.php
@@ -1,6 +1,47 @@
 <footer>
-            <p>&copy; 2025 Dragon Core. All rights reserved.</p>
-        </footer>
+    <div class="container footer-container">
+        
+        <div class="footer-col footer-info">
+            <p class="footer-title">BẤT CỨ ĐIỀU GÌ BẠN CẦN HỖ TRỢ, HÃY LIÊN HỆ VỚI CHÚNG TÔI NHÉ!</p>
+            <div class="logo-footer">DRAGON CORE</div>
+            <p><i class="fa-solid fa-location-dot"></i> Địa chỉ: Số 4 ngõ 133 Thái Hà, P.Trung Liệt, Q.Đống Đa, Hà Nội</p>
+            <p><i class="fa-solid fa-phone-volume"></i> Điện thoại: 024 6297 9999 - 024 6686 7777</p>
+            <p><i class="fa-solid fa-envelope"></i> Email: kinhdoanh@dragoncore.vn</p>
+
+            <div class="social-icons">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+
+        <div class="footer-col footer-policy">
+            <p class="footer-title">CHÍNH SÁCH CHUNG</p>
+            <ul>
+                <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Chính sách bảo hành</a></li>
+                <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Chính sách bảo mật</a></li>
+                <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Chính sách đổi trả hàng</a></li>
+                <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Chính sách vận chuyển</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-col footer-guide">
+            <p class="footer-title">HƯỚNG DẪN KHÁCH HÀNG</p>
+            <ul>
+                <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Hướng dẫn mua hàng trực tuyến</a></li>
+                <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Hướng dẫn mua hàng trả góp</a></li>
+                <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Hướng dẫn thanh toán</a></li>
+                <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Hướng dẫn khác</a></li>
+            </ul>
+        </div>
+
+    </div>
+    
+    <div class="footer-bottom">
+        <p>&copy; 2025 Dragon Core. All rights reserved.</p>
+        <p>Mã số thuế: 0107065190 - Do sở kế hoạch và đầu tư thành phố Hà Nội Cấp ngày 26/10/2015</p>
+    </div>
+</footer>
     </div>
 </body>
 </html>
